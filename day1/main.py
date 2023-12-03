d0d06a9ff5f41b9a07268c0ed67ed5882424004f
tmek1244 <tmek1244@wp.pl>
2023-12-03 12:54:54 +0100

    First day

--- day1/main.py
+++ day1/main.py
@@ -1,14 +1,19 @@
-def task1(input):
-    result = 0
-    for line in input:
-        a = list(filter(lambda x: ord(x) > 48 and ord(x) < 58, line))
-        result += int(a[0] + a[-1])
-    print(result)
+def task1(i):
+    a = lambda y: list(filter(lambda x: x.isdigit(), y))
+    return sum([int(a(l)[0]+a(l)[-1]) for l in i])
 
 
-def task2(input):
-    for line in input:
+def task2(i):
+    import re
+    d = ["one", "two", "three", "four",
+         "five", "six", "seven", "eight", "nine"]
 
+    def g(l):
+        r = re.findall(fr'(?=(\d|{"|".join(d)}))', l)
+        f = lambda x: str(d.index(x) + 1) if x in d else x
+        return int(f(r[0]) + f(r[-1]))
+
+    return sum([g(l) for l in i])
 
 
 def read_input():
@@ -18,5 +23,5 @@ def read_input():
 
 if __name__ == '__main__':
     input = read_input()
-    task1(input)
-    task2(input)
+    print(task1(input))
+    print(task2(input))
