6507c3975c542ae89939f81c27b14c70fc6280f5
tmek1244 <tmek1244@wp.pl>
2023-12-08 23:42:54 +0100

    Day 8

--- day8/main.py
+++ day8/main.py
@@ -25,25 +25,24 @@ def parse_input(input):
         else:
             nodes[node_id].left = nodes[children[0]]
             nodes[node_id].right = nodes[children[1]]
-    
+
     nodes_end = {x: "" for x in nodes.keys()}
 
     for node_id, node in nodes.items():
         current_node = node_id
         for turn in path:
-            # print(turn, current_node)
             if turn == 'L':
                 current_node = nodes[current_node].left.id
             else:
                 current_node = nodes[current_node].right.id
         nodes_end[node_id] = current_node
-    
+
     return nodes_end, path
 
 
 def task1(input):
     nodes_end, path = parse_input(input)
-    
+
     steps = 0
     current_node = "AAA"
     while current_node != "ZZZ":
@@ -53,36 +52,106 @@ def task1(input):
     return steps
 
 
+def find_cycle(nodes_edges, star_node):
+    visited = []
+    good_nodes = []
+    current_node = star_node
+    while True:
+        if current_node in visited:
+            return (
+                visited.index(current_node),
+                len(visited) - visited.index(current_node),
+                [node - visited.index(current_node) for node in good_nodes]
+            )
+        visited.append(current_node)
+        if current_node[-1] == "Z":
+            good_nodes.append(len(visited) - 1)
+        current_node = nodes_edges[current_node]
+
+
+def combine_phased_rotations(a_period, a_phase, b_period, b_phase):
+    """Combine two phased rotations into a single phased rotation
+
+    Returns: combined_period, combined_phase
+
+    The combined rotation is at its reference point if and only if both a and b
+    are at their reference points.
+    """
+    gcd, s, t = extended_gcd(a_period, b_period)
+    phase_difference = a_phase - b_phase
+    pd_mult, pd_remainder = divmod(phase_difference, gcd)
+    if pd_remainder:
+        raise ValueError("Rotation reference points never synchronize.")
+
+    combined_period = a_period // gcd * b_period
+    combined_phase = (a_phase - s * pd_mult * a_period) % combined_period
+    return combined_period, combined_phase
+
+
+def arrow_alignment(red_len, green_len, advantage):
+    """Where the arrows first align, where green starts shifted by advantage"""
+    period, phase = combine_phased_rotations(
+        red_len, 0, green_len, -advantage % green_len
+    )
+    return -phase % period
+
+
+def extended_gcd(a, b):
+    """Extended Greatest Common Divisor Algorithm
+
+    Returns:
+        gcd: The greatest common divisor of a and b.
+        s, t: Coefficients such that s*a + t*b = gcd
+
+    Reference:
+        https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode
+    """
+    old_r, r = a, b
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+    while r:
+        quotient, remainder = divmod(old_r, r)
+        old_r, r = r, remainder
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+
+    return old_r, old_s, old_t
+
+
 def task2(input):
     nodes_end, path = parse_input(input)
 
-    steps = 0
-    currents_nodes = []
-    for node_id in nodes_end.keys():
-        if node_id[-1] == 'A':
-            currents_nodes.append(node_id)
+    nodes_values = {x: (0, 0) for x in nodes_end.keys()}
 
-    sum = 0
-    for node in nodes_end.keys():
-        if node[-1] == "Z":
-            sum += 1
-    print(sum)
+    for node_id in nodes_end.keys():
+        r, circle_len, good_nodes = find_cycle(nodes_end, node_id)
+
+        if len(good_nodes) > 1:
+            raise Exception("More than one good node")
+        if len(good_nodes) == 0:
+            continue
+        sum = r + good_nodes[0]
+        nodes_values[node_id] = (sum, circle_len)
+
+    nodes_for_search = []
+    for node_id, node_value in nodes_values.items():
+        if node_id[-1] == "A":
+            nodes_for_search.append((node_value[0], node_value[1]))
+
+    node_ = nodes_for_search[0]
+    current_sum = node_[0]
+    step = node_[1]
+    idx = 1
     while True:
-        # print(currents_nodes)
-        for node in currents_nodes:
-            if node[-1] != "Z":
+        for node in nodes_for_search[idx:]:
+            if 0 != (current_sum - node[0]) % node[1]:
+                current_sum, step = combine_phased_rotations(current_sum, step, node[0], node[1])
+                idx += 1
                 break
         else:
             break
-        steps += len(path)
 
-        new_currents_nodes = []
-        for node in currents_nodes:
-            new_currents_nodes.append(nodes_end[node])
-
-        currents_nodes = new_currents_nodes
-    
-    return steps
+    return current_sum * len(path)
 
 
 def read_input():
@@ -92,5 +161,5 @@ def read_input():
 
 if __name__ == '__main__':
     input = read_input()
-    print(task1(input))
+    # print(task1(input))
     print(task2(input))
